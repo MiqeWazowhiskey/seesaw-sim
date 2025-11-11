82733303a79c81e7795a27182cadd01a1dc7ac2d
test <kaganylmz12@gmail.com>
2025-11-11 22:24:18 +0300

    update: add dummy weight preview

--- ALİKAGAN_YİLMAZ.js
+++ ALİKAGAN_YİLMAZ.js
@@ -1,6 +1,7 @@
 const playground = document.querySelector(".seesaw-playground");
 const starterText = document.querySelector(".starter-text");
 const restartButton = document.querySelector(".reset-button");
+const preWeight = document.querySelector(".weight-preview");
 
 let gameStarted = false;
 playground.classList.add("blink");
@@ -17,10 +18,22 @@ function resetGame() {
   playground.classList.add("blink");
   if (starterText) starterText.style.display = "block";
 }
+
 playground.addEventListener("click", function handler() {
   startGame();
 });
 
 restartButton.addEventListener("click", function () {
   resetGame();
+});
+
+playground.addEventListener("mousemove", (e) => {
+  const rect = playground.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  preWeight.style.display = "flex";
+  preWeight.style.left = `${x - preWeight.offsetWidth / 2}px`;
+});
+
+playground.addEventListener("mouseleave", () => {
+  preWeight.style.display = "none";
 });
